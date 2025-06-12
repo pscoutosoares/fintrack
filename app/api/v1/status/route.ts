b33d899439fea1026de98083ea3741ea2f03dc55
Pedro Soares <pscoutosoares@gmail.com>
2025-06-12 09:32:01 -0300

    feat: database and prismaORM configuration

--- app/api/v1/status/route.ts
+++ app/api/v1/status/route.ts
@@ -1,32 +1,28 @@
-// import { createRouter } from "next-connect";
+import { Prisma } from "@prisma/client";
 import database from "infra/database";
-import controller from "infra/controller";
 import { NextRequest, NextResponse } from "next/server";
-
-// const router = createRouter();
-
-// router.get(getHandler);
-
-// export default router.handler(controller.errorHandlers);
 
 export async function GET(request: NextRequest) {
   const updatedAt = new Date().toISOString();
   const maxConnections = await database.query("SHOW max_connections;");
   const currentConnections = await database.query({
-    text: `SELECT count(*)::int FROM pg_stat_activity WHERE datname = '$1';`,
+    text: `SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1`,
     values: [process.env.POSTGRES_DB],
   });
   const pgCurrentVersion = await database.query("SHOW server_version");
-  return new Response({
-    status: 200,
-    body: {
+  return Response.json(
+    {
       updated_at: updatedAt,
       dependencies: {
         database: {
-          version: pgCurrentVersion.rows[0].server_version,
-          max_connections: maxConnections.rows[0].max_connections,
-        opened_connections: currentConnections.rows[0].count,
+          version: pgCurrentVersion[0].server_version,
+          max_connections: maxConnections[0].max_connections,
+          opened_connections: currentConnections[0].count,
+        },
       },
     },
-  });
+    {
+      status: 200,
+    },
+  );
 }
